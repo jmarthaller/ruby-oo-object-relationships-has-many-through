baf7004c8b84e5706482df9d41e14b439b194d87
Jacob Marthaller <marthaller.jr@gmail.com>
2020-11-21 12:43:37 -0500

    hasmanythrough lab 2

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,33 @@
-class Waiter
+require 'pry'
 
+class Waiter
+    attr_accessor :name, :experience
+
+    @@all = []
+    
+    def initialize(name, experience)
+        @name = name
+        @experience = experience
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select { |meal| meal.waiter == self }
+    end
+
+    def best_tipper
+        #binding.pry
+        sorted = meals.sort_by { |meal| meal.tip }
+        last = sorted.pop
+        last.customer
+       # binding.pry
+    end
 end
